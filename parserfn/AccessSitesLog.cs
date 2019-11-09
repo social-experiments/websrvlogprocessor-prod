ddb97e5f3a04fcfa72962ded65f42d7583ef29cb
Park Swatosh <swatopar@uw.edu>
2019-11-08 17:09:27 -0800

    Added Bandwidth to model

--- parserfn/AccessSitesLog.cs
+++ parserfn/AccessSitesLog.cs
@@ -12,5 +12,6 @@ namespace ParserFn
         public string Url { get; set; }
         public long MainModuleCount { get; set; }
         public long SubModuleCount { get; set; }
+        public long Bandwidth { get; set; }
     }
 }
